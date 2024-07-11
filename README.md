3a88c349ddc4744e10ee25f4afd34f57e6a66b9c
hapqe <konrad@hapkem.com>
2024-07-11 18:21:26 +0200

    readme and refactoring

---
+++ README.md
@@ -0,0 +1,22 @@
+# Algorithmen aus [Abenteuer Informatik](https://www.abenteuer-informatik.de/) in Haskell!
+
+
+Komplilierung: `cabal build`, die Binary befindet sich in  `./dist-newstyle/`
+
+Benutzung:
+```the abenteuer program
+
+abenteuer [COMMAND] ... [OPTIONS]
+
+Common flags:
+  -?     --help           Display help message
+  -V     --version        Print version information
+
+abenteuer tournament [OPTIONS]
+
+  -n=INT                  The numbers of elements to be sorted
+
+abenteuer backpack [OPTIONS]
+
+  -c     --chestsize=INT  How many slots the chest has 
+```
--- app/Backpack.hs
+++ app/Backpack.hs
@@ -34,7 +34,12 @@ expect :: Maybe a -> [Char] -> a
 expect x msg = fromMaybe (error msg) x
 
 fillSmallest :: Treasure -> [Chest] -> [Chest]
-fillSmallest smallest = map (concatMap (\x -> if length x == length (item smallest) then item smallest else x) . chunks (length $ item smallest))
+fillSmallest smallest = 
+    map (
+        concatMap 
+        (\x -> if length x == length (item smallest) then item smallest else x) .
+        chunks (length $ item smallest)
+    )
 
 -- from "https://stackoverflow.com/a/12882583"
 chunks :: Int -> [a] -> [[a]]
@@ -43,7 +48,11 @@ chunks n = takeWhile (not . null) . unfoldr (Just . splitAt n)
 
 chestValue :: [Treasure] -> Chest -> Int
 chestValue treasures chest =
-    let treasureValue t = (fst . flip expect "a treasure has to be non-empty!" . uncons $ item t, (fromIntegral $ value t :: Float) / fromIntegral (length $ item t))
+    let treasureValue t = (
+            fst 
+            . flip expect "a treasure has to be non-empty!"
+            . uncons $ item t, (fromIntegral $ value t :: Float) / fromIntegral (length $ item t)
+            )
         values = Map.fromList (('⬜', 0) : map treasureValue treasures)
     in round $ sum $ map (fromJust . flip Map.lookup values) chest
 
@@ -51,13 +60,21 @@ optimizeTreasure :: [Treasure] -> [Chest] -> Treasure -> [Chest]
 optimizeTreasure treasures chests treasure =
     let maxValue a b = if chestValue treasures a > chestValue treasures b then a else b
         split = splitAt (length $ item treasure) chests
-        check (a:as, b:bs) = a : optimizeTreasure treasures (as ++ maxValue (a ++ item treasure) b : bs) treasure
+        check (a:as, b:bs) = a
+            : optimizeTreasure 
+            treasures 
+            (as ++ maxValue (a ++ item treasure) b : bs) 
+            treasure
         check (_, _) = chests
     in check split
 
 fillTreasures :: [Treasure] -> Int -> [Chest]
 fillTreasures [] chestSize = emptyChests chestSize
-fillTreasures treasures@(smallest : other) chestSize = foldl (optimizeTreasure treasures) (fillSmallest smallest $ emptyChests chestSize) other
+fillTreasures treasures@(smallest : other) chestSize = 
+    foldl 
+        (optimizeTreasure treasures) 
+        (fillSmallest smallest $ emptyChests chestSize) 
+        other
 
 demonstrateFill :: Int -> IO ()
 demonstrateFill n = printChests $ fillTreasures exampleTreasures n
